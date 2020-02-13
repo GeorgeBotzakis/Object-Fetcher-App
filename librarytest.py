f80f25371168b948929e825c2f406658ecf44ad3
Ace <geob777@hotmail.com>
2020-02-13 02:35:55 +0000

    Added a virtualenv and installed tensorflow 2.0 with python3.7.5

--- librarytest.py
+++ librarytest.py
@@ -1,3 +1,19 @@
-import tensorflow
-print(tensorflow.__version__)
-print("helloaaaa")
+from __future__ import absolute_import, division, print_function, unicode_literals
+try:
+ import tensorflow.compat.v2 as tf
+except Exception:
+    pass 
+
+tf.enable_v2_behavior()
+print(tf.__version__)
+# print(tensorflow.__version__)
+# print("helloaaaa")
+
+### Functional Model API - Simple
+# from tensorflow.keras import Sequential 
+# from tensorflow.keras.layers import Dense
+
+# #define the model
+# model = Sequential()
+# model.add(Dense(10, input_shape=(8,)))
+# model.add(Dense(1))
